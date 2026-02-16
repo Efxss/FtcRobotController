27ab541b44157f8fb0ba62a68d5e82520b63fd33
Efxss <135909007+Efxss@users.noreply.github.com>
2026-02-16 18:03:32 -0500

    Maybe fix TeleOP

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RedTeleOP.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RedTeleOP.kt
@@ -489,7 +489,9 @@ class RedTeleOP : OpMode() {
             val slot = nextSlot()
             if (slot != -1) {
                 // (Optional) choose G vs P here; your current logic makes g>=110 also count as P first.
-                ord[slot] = if (g >= 110) "G" else "P"
+                val newOrd = ord.copyOf()
+                newOrd[slot] = if (g >= 110) "G" else "P"
+                ord = newOrd
 
                 advanceBowl(slot)
 
