2d9285135e2fef75a58f4e93f82639c1e0470dd1
Efxss <135909007+Efxss@users.noreply.github.com>
2026-09-18 22:24:13 -0400

    Maybe fixed the repeat thing

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/config/subSystem/FlowerSS.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/config/subSystem/FlowerSS.kt
@@ -9,7 +9,8 @@ class FlowerSS(robot: Robot) {
     private fun exe(robot: Robot): Command = Groups.sequential(
         Commands.instant { robot.flowerS.set(0.4) },
         Commands.waitMs(250.0).setPriority(10),
-        Commands.instant { robot.flowerS.set(0.0) }
+        Commands.instant { robot.flowerS.set(0.0) },
+        Commands.waitMs(250.0).setPriority(10)
     )
     fun deFlower(robot: Robot): Command = Groups.repeat(exe(robot),3)
     fun reset(robot: Robot) = robot.flowerS.set(0.0)
