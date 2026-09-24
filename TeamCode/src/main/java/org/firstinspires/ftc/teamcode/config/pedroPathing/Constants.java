96adcf775fa43aff0f7e6b3315a71105ac68881d
Efxss <135909007+Efxss@users.noreply.github.com>
2026-09-24 15:13:10 -0400

    Readded the Mecanum code to the constants cause it should be the same as before

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/config/pedroPathing/Constants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/config/pedroPathing/Constants.java
@@ -1,9 +1,27 @@
 package org.firstinspires.ftc.teamcode.config.pedroPathing;
 
 import com.pedropathing.follower.Follower;
+import com.pedropathing.revhub.drivetrains.MecanumConfig;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 public class Constants {
+    public static MecanumConfig drivetrainConfig = new MecanumConfig(
+            c -> {
+                c.frontLeftName.set("frontLeft");
+                c.backLeftName.set("backLeft");
+                c.frontRightName.set("frontRight");
+                c.backRightName.set("backRight");
+
+                c.frontLeftDirection.set(DcMotorSimple.Direction.REVERSE);
+                c.backLeftDirection.set(DcMotorSimple.Direction.REVERSE);
+                c.frontRightDirection.set(DcMotorSimple.Direction.FORWARD);
+                c.backRightDirection.set(DcMotorSimple.Direction.FORWARD);
+
+                c.manualBrakeMode.set(true);
+                c.powerThreshold.set(0.03);
+            }
+    );
     public static Follower create(HardwareMap h) {
         // return new Follower(Drivetrain, Localizer, Foresight);
         return null;
