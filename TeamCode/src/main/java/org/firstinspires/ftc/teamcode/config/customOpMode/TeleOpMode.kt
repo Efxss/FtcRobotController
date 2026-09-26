5de7b34d1703571c27d3df26c7ad542b8453503e
Efxss <135909007+Efxss@users.noreply.github.com>
2026-09-26 14:42:56 -0400

    Checked the loop times and found they are good so I stopped tracking them

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/config/customOpMode/TeleOpMode.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/config/customOpMode/TeleOpMode.kt
@@ -7,7 +7,6 @@ import com.pedropathing.follower.ManualDrive
 import com.pedropathing.ivy.Scheduler
 import com.pedropathing.math.Pose
 import com.qualcomm.robotcore.eventloop.opmode.OpMode
-import com.qualcomm.robotcore.util.ElapsedTime
 import org.firstinspires.ftc.teamcode.config.Robot
 import org.firstinspires.ftc.teamcode.config.subSystem.FlowerSS
 import org.firstinspires.ftc.teamcode.config.subSystem.IntakeSS
@@ -29,7 +28,6 @@ abstract class TeleOpMode : OpMode() {
     protected lateinit var intakeSS: IntakeSS
     protected lateinit var flowerSS: FlowerSS
     protected var resetPose = Pose(9.0, 8.8, Math.toRadians(90.0))
-    lateinit var elapsedTime: ElapsedTime
     protected var dp: DrivePowers = ManualDrive.fieldCentric(
         0.0,
         0.0,
@@ -71,7 +69,6 @@ abstract class TeleOpMode : OpMode() {
 
     final override fun init() {
         // Any other shared init (hardware caching, subsystems, etc.)
-        elapsedTime = ElapsedTime()
 
         // declare Panels and init the debug util
         panels = PanelsTelemetry.telemetry
@@ -102,9 +99,8 @@ abstract class TeleOpMode : OpMode() {
             Robot.Alliance.BLUE -> { Pose(9.0, 8.8, Math.toRadians(90.0)) }
             Robot.Alliance.RED -> { Pose(132.3, 132.7, Math.toRadians(90.0)) }
         }
-        //Scheduler.schedule(intakeSS.runIntake())
+        Scheduler.schedule(intakeSS.runIntake())
         //robot.genTab()
-        elapsedTime.reset()
         onStart()
     }
 
@@ -141,8 +137,7 @@ abstract class TeleOpMode : OpMode() {
         Scheduler.execute()
 
         //Show and update debug
-        debugUtil.showAllDebugTeleop(robot.follower,alliance,runtime,gamepad1,flowerSS,elapsedTime,robot)
-        elapsedTime.reset()
+        debugUtil.showAllDebugTeleop(robot.follower,alliance,runtime,gamepad1,flowerSS,robot)
         debugUtil.update(telemetry)
         onLoop()
     }
